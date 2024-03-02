27939d2e1d75cc4bae6d25afb7fbfab0b5dc740c
Tee Johnson <lexjox777@gmail.com>
2024-03-02 15:54:28 +0000

    uber pickups app added and file updated

---
+++ uber_pickups.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+ 
+st.title("Uber pickups in NYC")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# data_load_state.text("Done! (using st.cache_data)")
+
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+
+st.write("Most objects") # df, err, func, keras!
+st.write(["st", "is <", 3]) # see *
+st.write_stream(my_generator)
+st.write_stream(my_llm_stream)
+
+st.text("Fixed width text")
+st.markdown("_Markdown_") # see *
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+* optional kwarg unsafe_allow_html = True
